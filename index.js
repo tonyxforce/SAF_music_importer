9e662f94eaabde94dea2ed91e2623a819066ee34
tonyxforce <horich.dominik@gmail.com>
2024-04-13 20:53:31 +0200

    make it compatible

--- index.js
+++ index.js
@@ -2,28 +2,35 @@ const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-var folder = ".\\" || "C:\\Users\\Horic\\AppData\\Roaming\\.minecraft\\music\\"
-
-var inputDir = "input";
+var inputDir = process.env.APPDATA + "\\.minecraft\\music";
 var outputDir = "output";
 
-fs.readdirSync("input", {withFileTypes:true,recursive:true}).forEach((e)=>{
+var outJson = {};
+
+if(!fs.existsSync(outputDir))
+fs.mkdirSync(outputDir);
+
+fs.readdirSync(inputDir, {withFileTypes:true,recursive:true}).forEach((e)=>{
 	if(!e.isFile()) return;
 	e.nameNoPath = e.name.replace(/\.[^/\\.]+$/, "");
 
 
-	var outNameLow = `${outputDir}\\${e.nameNoPath}_sub.mp3`
+	var outNameLow = `${outputDir}\\${e.nameNoPath}_sub.ogg`
 	if(fs.existsSync(outNameLow))
 	fs.unlinkSync(outNameLow)
 
 
-	var outNameMid = `${outputDir}\\${e.nameNoPath}_mid.mp3`
+	var outNameMid = `${outputDir}\\${e.nameNoPath}_mid.ogg`
 	if(fs.existsSync(outNameMid))
 	fs.unlinkSync(outNameMid)
 
-	if(!fs.existsSync("output"))
-	fs.mkdirSync("output");
+	outJson[`${e.nameNoPath}_sub`] = {sounds:["custom:" + e.nameNoPath + "_sub"]};
+	outJson[`${e.nameNoPath}_mid`] = {sounds:["custom:" + e.nameNoPath + "_mid"]};
 
-	child_process.execSync(`ffmpeg -i ${inputDir}\\${e.name} -af lowpass=120 ${outNameLow}`);
-	child_process.execSync(`ffmpeg -i ${inputDir}\\${e.name} -af highpass=120 ${outNameMid}`);
+
+
+	child_process.execSync(`ffmpeg -i "${inputDir}\\${e.name}" -af lowpass=120 -ac 1 "${outNameLow}"`);
+	child_process.execSync(`ffmpeg -i "${inputDir}\\${e.name}" -af highpass=120 -ac 1 "${outNameMid}"`);
 })
+
+fs.writeFileSync(outputDir + "/out.json", JSON.stringify(outJson, null, 4));
