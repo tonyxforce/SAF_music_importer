50411468cca21f1ca6746fb302061496b66c2480
tonyxforce <horich.dominik@gmail.com>
2024-04-13 20:41:26 +0200

    make it actually work

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const { execSync } = require('child_process');
-const { unlinkSync, readdirSync, existsSync } = require('fs');
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 var folder = ".\\" || "C:\\Users\\Horic\\AppData\\Roaming\\.minecraft\\music\\"
 
 var inputDir = "input";
 var outputDir = "output";
 
-readdirSync("input", {withFileTypes:true,recursive:true}).forEach((e)=>{
+fs.readdirSync("input", {withFileTypes:true,recursive:true}).forEach((e)=>{
 	if(!e.isFile()) return;
+	e.nameNoPath = e.name.replace(/\.[^/\\.]+$/, "");
 
 
-	var outName = `${outputDir}\\${e.name}_sub.mp3`
-	if(existsSync(outName))
-	unlinkSync(outName)
+	var outNameLow = `${outputDir}\\${e.nameNoPath}_sub.mp3`
+	if(fs.existsSync(outNameLow))
+	fs.unlinkSync(outNameLow)
 
-	execSync(`ffmpeg -i ${inputDir}\\${e.name} -af highpass=120 ${outName}`);
+
+	var outNameMid = `${outputDir}\\${e.nameNoPath}_mid.mp3`
+	if(fs.existsSync(outNameMid))
+	fs.unlinkSync(outNameMid)
+
+	if(!fs.existsSync("output"))
+	fs.mkdirSync("output");
+
+	child_process.execSync(`ffmpeg -i ${inputDir}\\${e.name} -af lowpass=120 ${outNameLow}`);
+	child_process.execSync(`ffmpeg -i ${inputDir}\\${e.name} -af highpass=120 ${outNameMid}`);
 })
